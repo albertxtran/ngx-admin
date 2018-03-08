dab5f3569eb736e13af145947072b79f535f12f6
Raj Desai <rddesai@us.ibm.com>
2018-03-07 22:09:59 -0800

    Syntax fixes

--- src/app/pages/portfolio/portfolio.module.ts
+++ src/app/pages/portfolio/portfolio.module.ts
@@ -3,7 +3,7 @@ import { CommonModule }  from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Ng2PaginationModule } from 'ng2-pagination'; 
 
-import { PortfolioComponent, SearchPipe, PipeFilter, SearchArrayPipe } from './portfolio.component';
+import { PortfolioComponent, SearchArrayPipe } from './portfolio.component';
 import { PortfolioService } from './portfolio.service';
 
 import { ThemeModule } from '../../@theme/theme.module';
@@ -20,8 +20,6 @@ import { PortfolioRoutingModule, routedComponents } from './portfolio-routing.mo
   declarations: [
     ...routedComponents,
     PortfolioComponent,
-    SearchPipe,
-    PipeFilter,
     SearchArrayPipe
   ],
   providers: [
