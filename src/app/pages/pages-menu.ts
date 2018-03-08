03bebabb4ffc1725850ce75a14c2fa608bdcb97e
Raj Desai <rddesai@us.ibm.com>
2018-03-08 10:46:00 -0800

    Removed unused pages from menu

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -1,6 +1,10 @@
 import { NbMenuItem } from '@nebular/theme';
 
+
 export const MENU_ITEMS: NbMenuItem[] = [
+
+  // DO NOT USE THIS ANYMORE, use ./_menu/*-menu.ts
+
   {
     title: 'Dashboard',
     icon: 'nb-home',
